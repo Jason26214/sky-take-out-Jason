a43f0f4eb1ff7304184993dd9958cc5dcf403d61
Jason Z <jason26214@gmail.com>
2024-09-01 03:48:12 +0800

    Dish PageQuery

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,27 +1,35 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface DishMapper {
-
     /**
      * 根据分类id查询菜品数量
-     * @param categoryId
-     * @return
+     * @param categoryId categoryId
+     * @return Integer
      */
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
-
     /**
      * 插入菜品数据(XML)
-     * @param dish
+     * @param dish dish
      */
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+
+    /**
+     * 菜品分页查询(XML)
+     * @param dishPageQueryDTO dishPageQueryDTO
+     * @return PageResult
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
