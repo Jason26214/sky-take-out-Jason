c986230aaf9b2c30aa725f53035f055d60822ace
Jason Z <jason26214@gmail.com>
2024-09-10 22:49:32 +0800

    menu browsing

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -11,12 +11,14 @@ import java.util.List;
 public interface DishService {
     /**
      * 新增菜品和对应口味
+     *
      * @param dishDTO
      */
     void saveWithFlavor(DishDTO dishDTO);
 
     /**
      * 菜品分页查询
+     *
      * @param dishPageQueryDTO dishPageQueryDTO
      * @return PageResult
      */
@@ -24,12 +26,14 @@ public interface DishService {
 
     /**
      * 菜品批量删除
+     *
      * @param ids
      */
     void deleteBatch(List<Long> ids);
 
     /**
      * 根据id查询菜品和对应的口味数据
+     *
      * @param id
      * @return
      */
@@ -37,12 +41,14 @@ public interface DishService {
 
     /**
      * 根据id修改菜品基本信息和对应的口味信息
+     *
      * @param dishDTO
      */
     void updateWithFlavor(DishDTO dishDTO);
 
     /**
      * 菜品起售/停售
+     *
      * @param status
      * @param id
      */
@@ -50,8 +56,17 @@ public interface DishService {
 
     /**
      * 根据分类id查询菜品
+     *
      * @param categoryId
      * @return
      */
     List<Dish> list(Long categoryId);
+
+    /**
+     * 条件查询菜品和口味
+     *
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
