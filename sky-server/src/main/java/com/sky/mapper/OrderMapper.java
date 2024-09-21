a5e7fe34ab44d65c6e7bdd3c5c65efcb5a480116
Jason Z <jason26214@gmail.com>
2024-09-22 03:15:27 +0800

    Revenue Data Statistics

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -86,4 +87,11 @@ public interface OrderMapper {
      */
     @Select("SELECT * FROM orders WHERE status = #{status} and order_time < #{orderTime}")
     List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
+    /**
+     * 根据动态条件统计营业额数据
+     * @param map
+     * @return
+     */
+    Double sumByMap(Map<String, Object> map);
 }
